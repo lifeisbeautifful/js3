e6128783385499cc3f328a63f04d7b489c2301c2
Оксана <oksankagnyp@gmail.com>
2018-09-02 14:33:16 +0000

    ог

---
+++ text.js
@@ -0,0 +1,35 @@
+let arg1=parseFloat(prompt("Введіть аргумент a"));
+
+arg2=parseFloat(prompt("Введіть аргумент b"));
+argq2=Math.pow(arg2,2);
+
+arg3=parseFloat(prompt("Введіть аргумент c"));
+
+meat4=parseFloat(4*arg1*arg3);
+
+d=parseFloat(argq2-meat4);
+
+qd=Math.sqrt(d);
+
+deno=parseFloat(2*arg1);
+
+before1=divide(-arg2+qd);
+//alert(before1);
+
+
+
+
+function divide(number){
+	 return number/deno;
+	
+
+}
+
+before2=divide2(deno);
+//alert(before2);
+
+function divide2(numb){
+return (-arg2-qd)/numb;}
+
+alert("х1="+before1);
+alert("x2="+before2);
